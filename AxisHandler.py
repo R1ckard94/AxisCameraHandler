0bad31af5b60afbfa61f8541cb689fcf7731dc23
Rika1700 <78212506+Rika1700@users.noreply.github.com>
2022-04-20 14:34:26 +0200

    funkar mot localhost

--- AxisHandler.py
+++ AxisHandler.py
@@ -7,12 +7,12 @@ def on_connect(client, userdata, flags, rc):
     client.subscribe("#")
 
 def on_message(client, userdata, msg):
-    print(msg.topic+" :: "+str(msg.payload))
+    print(msg.topic+" :: "+str(msg.payload)) #får konstigt " b'payload' "
 
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
-
+client.username_pw_set("user", "user123")
 client.connect("localhost", 1883, 60)
 
 client.loop_forever()
