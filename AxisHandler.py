14326d02ea4446fe1d52ce285442d7cc9cea2534
Rika1700 <78212506+Rika1700@users.noreply.github.com>
2022-04-21 21:58:56 +0200

    dont need threads because the mqtt broker will handle overcrowdning for me

--- AxisHandler.py
+++ AxisHandler.py
@@ -1,18 +1,35 @@
 from multiprocessing.connection import Client
 import paho.mqtt.client as mqtt
+import  json, time
+
+
+
+
+def sendToApi(jsonMsg):
+    print()
+
+def isValid(msg):
+    #behöver ingen kö för att även om mitt program är segt så kommer brokern skicka ut det som kommer fram till den ändå
+    print()
+    
 
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
-
+    if(rc != 0):
+        print("Connection code "+str(rc))
     client.subscribe("#")
 
 def on_message(client, userdata, msg):
-    print(msg.topic+" :: "+str(msg.payload)) #får konstigt " b'payload' "
+    #print(msg.topic+" :: "+str(msg.payload)) #får konstigt " b'payload' "
+    print("thread activitiy")
+    #handlingQueue.put(str(msg.payload))
+    time.sleep(4)
+    print(msg.topic+" :: "+str(msg.payload))
+
+
 
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 client.username_pw_set("user", "user123")
 client.connect("localhost", 1883, 60)
-
 client.loop_forever()
