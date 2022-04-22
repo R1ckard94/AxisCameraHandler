35555d5fb5f84fe4eb66fc9d3aa25fdb02028430
Rika1700 <78212506+Rika1700@users.noreply.github.com>
2022-04-22 13:55:28 +0200

    rå data för xMotion funkar för att läsa av när en person går in och ut

--- AxisHandler.py
+++ AxisHandler.py
@@ -3,15 +3,33 @@ import paho.mqtt.client as mqtt
 import  json, time
 
 
-
-
-def sendToApi(jsonMsg):
+def sendToApi(jsonMsg): #det som sållas fram av "isValid"
     print()
 
-def isValid(msg):
+def isValid(jsonMsg):
     #behöver ingen kö för att även om mitt program är segt så kommer brokern skicka ut det som kommer fram till den ändå
-    print()
-    
+    if(jsonMsg["class"] == 'Person'):
+        #print("diz is person")
+        tmp = jsonMsg["path"]
+        tmpArr = []
+        for tm in tmp:
+            tmpArr.append(tm["x"])
+        #print(tmpArr)
+        if(tmpArr[0] < tmpArr[-1]): #går från vänster till höger 
+            if(tmpArr[-1] > 250): #måste nog ändra så att storleken blir mindre eller större beroende på data som kommer in
+                 print("en person har gått ut!")
+                 return
+        if(tmpArr[0] > tmpArr[-1]):
+            if(tmpArr[0] < 250):
+                print("en person har gått in!")
+                return
+
+    if(jsonMsg["class"] == 'Vehicle'):
+        print("diz is Vehicle")
+        tmp = jsonMsg["path"]
+        for tm in tmp:
+            print(tm["x"])
+
 
 def on_connect(client, userdata, flags, rc):
     if(rc != 0):
@@ -20,11 +38,9 @@ def on_connect(client, userdata, flags, rc):
 
 def on_message(client, userdata, msg):
     #print(msg.topic+" :: "+str(msg.payload)) #får konstigt " b'payload' "
-    print("thread activitiy")
-    #handlingQueue.put(str(msg.payload))
+    #print("thread activitiy")
+    isValid(json.loads(msg.payload))
     time.sleep(4)
-    print(msg.topic+" :: "+str(msg.payload))
-
 
 
 client = mqtt.Client()
